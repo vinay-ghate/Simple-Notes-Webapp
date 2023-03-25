48fe058acffe7c8aec267827f6ee85aa740eaaa1
Vinay Ghate <47355614+vinay-ghate@users.noreply.github.com>
2023-03-25 23:26:47 +0530

    Saved Branch

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
 
 @auth.route('/login')
 def login():
-    return '<p>Login</p>'
+    return render_template("login.html", boolean =True)
+
 
 @auth.route('/logout')
 def logout():
     return '<p>Log Out</p>'
 
+
+
 @auth.route('/sign-up')
 def sign_up():
-    return '<p>Sign Up</p>'
+    return render_template("sign_up.html")
