d64ff0ea0fcf273d50a5b0110035756042950faa
Vinay Ghate <47355614+vinay-ghate@users.noreply.github.com>
2023-03-26 03:22:11 +0530

    Cmplited

--- website/__init__.py
+++ website/__init__.py
@@ -4,25 +4,26 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
+DB_NAME = "database.db"
+
 
 def create_app():
-    app = Flask(__name__) 
-    app.config['SECRET_KEY'] = 'spider manus'  #used for encryption on cookies
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views,url_prefix='/')
-    app.register_blueprint(auth,url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-
-    create_database(app)
     
+    with app.app_context():
+        db.create_all()
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -31,12 +32,10 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-
-
     return app
+
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        with app.app_context():
-            db.create_all()
+        db.create_all(app=app)
         print('Created Database!')
--- website/models.py
+++ website/models.py
@@ -11,8 +11,8 @@ class Note(db.Model):
 
 
 class User(db.Model, UserMixin):
-    id= db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(150),unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')   #Note is class above
+    notes = db.relationship('Note')
