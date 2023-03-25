d25c2834c240e5abeb37a2fbb14fcc56dd442a3f
Vinay Ghate <47355614+vinay-ghate@users.noreply.github.com>
2023-03-26 00:39:01 +0530

    At sign in page base.html added flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request ,flash
 
 auth = Blueprint('auth',__name__)
 
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
     return render_template("login.html", boolean =True)
 
@@ -13,7 +13,24 @@ def logout():
     return '<p>Log Out</p>'
 
 
-
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 4 Characters.",category='error')
+        elif len(firstName) < 1:
+            flash("FirstName must be greater than 1 Characters.",category='error')
+        elif password1 != password2:
+            flash("Passwords Don't match.",category='error')
+        elif len(password1) < 7:
+            flash("Password must be greater than 7+ Characters.",category='error')
+        else:
+            flash("Account created!",category='success')
+
+            
     return render_template("sign_up.html")
