b39dcc06e04bc5bec53ca8f473291a486b1e047f
vinay <ghatevinay2@gmail.com>
2023-10-15 01:38:11 +0530

    A

--- website/auth.py
+++ website/auth.py
@@ -16,7 +16,7 @@ def login():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            if check_password_hash(user.password, password):
+            if user.password == password:#check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
@@ -55,8 +55,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=password1) #generate_password_hash( password1, method='sha256')
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
